d3c2eab991055823e711bbfd3a00fbd12a6b3980
victorpessoa01 <auserofficial007@gmail.com>
2022-10-10 09:46:00 -0300

    Realizei a programação de cada botão e finalizei o designer da Calculadora. Já está totalmete funci0nal.

--- Calculadora/Form1.Designer.cs
+++ Calculadora/Form1.Designer.cs
@@ -324,6 +324,7 @@
             this.Controls.Add(this.button1);
             this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
             this.Name = "FormCal";
             this.Text = "Calculadora";
